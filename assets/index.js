fcdf136a0947ad4dd45eec048bd96fad9ad984cf
Philip Rodrigues <Malvado996@gmail.com>
2020-06-25 23:16:58 -0400

    added asset and sass folders

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "../client/App";
+
+import styles from "./scss/styles.scss";
+
+
+render(<App />, document.getElementById("root"));
