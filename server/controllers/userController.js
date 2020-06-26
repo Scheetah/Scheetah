d1b6dc641a8cb74371f5c84b3e0c6936db5ac780
Anthony <anthonylee2797@gmail.com>
2020-06-26 13:34:39 -0400

    added create/delete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,8 @@ const User = require('../userModel');
 
 const userController = {
 
+  //User Create
+
   createUser(req, res) {
     console.log('testing')
 
@@ -13,13 +15,15 @@ const userController = {
     User.create({
         username,
         password,
-        data: [1, 2, 3, 4, 5]
+        data: []
       })
       .then(data => res.status(200).json(data))
       .catch(err => res.status(418).json({
         error: 'Error adding user'
       }))
   },
+
+  //Read User
 
   getUser(req, res) {
     const user = req.params.username;
@@ -32,6 +36,36 @@ const userController = {
       .catch(err => res.status(418).json({
         error: 'Cannot find user by that name'
       }))
+  },
+
+  //what we have
+  //req.body.username
+  //req.body.text
+  //req.body.column
+
+  //post 
+  //udpate the schema data
+
+  //create Note
+  createNote(req, res) {
+    console.log('creatingnote!')
+    const { username, text, column} = req.body;
+
+    const filter = {username};
+    const update = {data: 'hello'};
+    User.findOneAndUpdate(filter,update, ((err, result) => {
+      if (err){
+        res.sendStatus(418)
+      } else {
+        res.sendStatus(200)
+      }
+    }));
+    
+  },
+
+  //delete Note
+  deleteNote(req, res){
+
   }
 }
 
