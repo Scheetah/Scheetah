91f7e95da29a41a2b87313f9960430a25cfc0373
Philip Rodrigues <Malvado996@gmail.com>
2020-06-25 22:58:45 -0400

    scss folder added/ must install npm loader package instructions in readme

--- client/index.js
+++ client/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
-import styles from "./styles.scss";
+import styles from "./scss/styles.scss";
 
 render(<App />, document.getElementById("root"));
